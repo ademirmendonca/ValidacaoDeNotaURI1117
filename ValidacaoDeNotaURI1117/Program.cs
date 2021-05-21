b4ae856557ef8b5f6cb138ca8d3647c9252c2b3d
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-05-21 15:46:37 -0300

    Add project files.

---
+++ ValidacaoDeNotaURI1117/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValidacaoDeNotaURI1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = 0, nota2 = 0, media = 0;
+
+            nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);            
+
+            //Verifica se a primeira nota informada e valida.
+            while (nota1 < 0 || nota1 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            //Verifica se a segunda nota informada e valida.
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota2 < 0 || nota2 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            //Calculo da media final e impressao da media com duas casas decimais.
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine("media = " + media.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
